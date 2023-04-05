d8b4e6acb1319259365650f67a88434f645835cd
Saddiqq <9adi8.tm@gmail.com>
2023-04-05 11:03:53 +0400

    Optical Character Recognition (OCR)

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+
+namespace OCR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new IronOcr.IronTesseract().Read(@"images\MSTestImg.png").Text);
+            Console.ReadKey();
+        }
+    }
+}
